5ebc8710e6023f2699b617daf2f5e44ddb9e6a76
pranay <pranayharishchandra@gmail.com>
2023-10-20 09:11:11 +0530

    getting started with Express.js

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const http = require('http')
+// const http = require('http')
 const fs   = require('fs')
 const index = fs.readFileSync('index.html', 'utf-8')
-const data = fs.readFileSync('data.json', 'utf-8')
+
+const data = JONS.parse(fs.readFileSync('data.json', 'utf-8'))
+
+const express = require('express') // to be writen after reading file
+const port = 8080
+
+const server = express() // app
 
 
-const server = http.createServer((req, res) => {
-    console.log('server started')
+server.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+  
 
-    // res.setHeader('Content-Type', 'text/html'); // will be ignored
-    // res.setHeader('Dummy', 'DummyValue'); 
-    res.setHeader('Content-Type', 'application/json') 
-    
-    // you can send response only once for a single http request
-    // const responseData = JSON.stringify(data) + '<h1>Hello, World!</h1>';
-    // res.end(responseData);
-    
-    // since we are 
-    res.end(data) // since we have read it in string only
-                 // so simply sending the string so no need for parsing
-    // res.end(JSON.stringify(data)) // if you would have parsed the data
 
+server.listen(port, () => {
+    console.log(`Server running, port: ${port}`);
 })
 
 
-server.listen(8080, () => {
-    console.log('Server is listening on port 8080');
-})
+
+
+
+
