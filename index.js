c6c6de39da525b45e72284bf4d10a4322fe64144
pranay <pranayharishchandra@gmail.com>
2023-10-23 00:03:04 +0530

    fetching and showing a product by replacing html

--- index.js
+++ index.js
@@ -1,44 +1,91 @@
-// const http = require('http')
+const http = require('http')
 const fs   = require('fs')
 const index = fs.readFileSync('index.html', 'utf-8')
 
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'))
 
-const express = require('express') // to be writen after reading file
 const port = 8080
- 
-const server = express() // app
 
 
-server.get('/', (req, res) => {
-    // res.send('Hello, Express!');
-    // res.end(JSON.stringify({type:'get'}))
-    res.end(JSON.stringify(data))
+const product = data.products[0]
 
-});
-  
-//  APU - Endpoint - Route
-server.put('/', (req, res) => {
-    res.json({type:'put'})
-});
-  
-server.delete('/', (req, res) => {
-    res.json({type:'delete'})
-});
- 
-server.patch('/', (req, res) => {
-    res.json({type:'patch'})
-});
- 
-server.post('/', (req, res) => {
-    res.json({type:'post'})
-});
-  
+const server = http.createServer((req,res) => {
+    console.log(req.url)
 
- 
+    switch (req.url){
+        case '/':
+            res.setHeader('Content-Type','text/html')
+            res.end(index) 
+            break
+            
+        case '/api':
+            res.setHeader('Content-Type','application/json')
+            res.end(data)
+            break
+        
+        case '/product':
+            res.setHeader('Content-Type','text/html')
+            let modifiedIndex = index.replace('**title**', product.title)
+                                     .replace('**url**', product.thumbnail)
+                                     .replace('**price**', product.price)
+                                     .replace('**rating**', product.rating)
+                                     .replace('**discount**', product.discountPercentage)
+            res.end(modifiedIndex)
+            break
+        
+        default:
+            // res.writeHead(404, {<headers>})
+            res.writeHead(404, 'NT FOUND')
+            res.end() // write noting
+            break
+    }
+})
+
+
+
+
 server.listen(port, () => {
     console.log(`Server running, port: ${port}`);
 })
+
+
+
+
+
+
+
+
+
+
+// const express = require('express') // to be writen after reading file
+// const server = express() // app
+
+// server.get('/', (req, res) => {
+    //     // res.send('Hello, Express!');
+    //     // res.end(JSON.stringify({type:'get'}))
+//     res.end(JSON.stringify(data))
+
+// });
+  
+// //  APU - Endpoint - Route
+// server.put('/', (req, res) => {
+//     res.json({type:'put'})
+// });
+  
+// server.delete('/', (req, res) => {
+//     res.json({type:'delete'})
+// });
+ 
+// server.patch('/', (req, res) => {
+//     res.json({type:'patch'})
+// });
+ 
+// server.post('/', (req, res) => {
+//     res.json({type:'post'})
+// });
+  
+
+ 
 
 
 
