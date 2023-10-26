ff8f9a8ff4b55a34a0cf9d9eccaf9dcae26f72cd
pranay <pranayharishchandra@gmail.com>
2023-10-26 22:06:54 +0530

    POST

--- index.js
+++ index.js
@@ -19,12 +19,35 @@ server.use(morgan('default'))
 server.use(express.json())
 
 
+
+
+// 1  Create POST /products    C R U D
+server.post('/products', (req, res) => {
+    // reading the created product in the body
+    // POST -> body -> raw (json) -> [req.body]
+    const new_product = req.body
+    console.log(JSON.stringify(new_product))
+    products.push(new_product)
+    res.json(new_product)
+})
+
+
     
 
-// Read GET 
+// 2 Read GET 
 server.get('/products', (req, res) => {
     res.json(products)
 })
+
+// 3 Update 
+server.put('./products/:id', (req, res) => {
+    const id = +req.params.id
+    const product = products.find(p => p.id === id)
+    res.json(product)
+})
+
+// 4 delete
+
 
 // Read GET /products/:id
 server.get('/products/:id', (req, res) => {
@@ -33,24 +56,6 @@ server.get('/products/:id', (req, res) => {
     const product = products.find((p) => p.id === id)
     res.json(product)
 })
-
-
-
-
-// Create POST /products 
-server.post('/products', (req, res) => {
-    // reading the created product in the body
-    // POST -> body -> raw (json) -> [req.body]
-    const new_product = req.body
-    console.log(JSON.stringify(new_product))
-    products.push(new_product)
-
-    // Write the updated products array to the JSON file
-    // fs.writeFileSync('data.json', JSON.stringify(products, null, 2));
-
-    res.json(new_product)
-})
-
 
 
 
